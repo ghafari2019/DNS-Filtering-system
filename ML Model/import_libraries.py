51470a5b8970f1a8a8791d55ceb7b7b8f548748a
ghafari2019 <50988702+ghafari2019@users.noreply.github.com>
2024-06-28 16:42:55 +0200

    Create import_libraries.py

---
+++ ML Model/import_libraries.py
@@ -0,0 +1,42 @@
+# Import necessary libraries
+import re
+import string
+import logging
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from urllib.parse import urlparse
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, classification_report, confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier, GradientBoostingClassifier, BaggingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import SGDClassifier, LogisticRegression, RidgeClassifier, Perceptron
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.mixture import GaussianMixture
+from sklearn.cluster import KMeans
+from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+from tldextract import extract as tld_extract
+from tld import get_tld, is_tld
+from tld.exceptions import TldDomainNotFound, TldBadUrl, TldIOError
+from colorama import Fore
+from datetime import datetime
+from plotly.subplots import make_subplots
+from plotly import graph_objects as go
+from wordcloud import WordCloud
+from gensim.models import Word2Vec
+import tldextract
+import hashlib
+import whois
+import warnings
+from sklearn.preprocessing import LabelEncoder
+import ipaddress
+from tabulate import tabulate
